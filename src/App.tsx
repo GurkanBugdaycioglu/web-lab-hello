5ba08dd008a852b142b4ba3667c5f9f205024e15
Gürkan Buğdaycıoğlu <gurkanbugdaycioglu1@gmail.com>
2026-02-25 18:59:58 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyadi: Gürkan Buğdaycıoğlu</p>
+      <p>Ogrenci No: 200541048</p>
+    </div>
+  )
+}
+
+export default App
