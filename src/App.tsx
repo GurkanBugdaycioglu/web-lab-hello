54b2c4acc5700a8125975bb8faf5ba6b8c18f4e9
Gürkan Buğdaycıoğlu <gurkanbugdaycioglu1@gmail.com>
2026-03-23 22:14:31 +0300

    feat: add semantic HTML portfolio structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 function App() {
   return (
-    <div>
-      <h1>Web Tasarımı ve Programlama</h1>
-      <h2>LAB-1</h2>
-      <p>Ad Soyad: Gürkan Buğdaycıoğlu</p>
-      <p>Öğrenci No: 200541048</p>
-      <p>Bölümü: Yazılım Mühendisliği</p>
-    </div>
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#hakkimda">Hakkimda</a></li>
+          <li><a href="#projeler">Projeler</a></li>
+          <li><a href="#iletisim">Iletisim</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+        <section id="hakkimda">
+          <h2>Hakkimda</h2>
+          <p>Merhaba, benim adım Gürkan Buğdaycıoğlu. Fırat Üniversitesi Teknoloji Fakültesi Yazılım Mühendisliği bölümünde okuyorum.</p>
+        </section>
+        <section id="projeler">
+          <h2>Projelerim</h2>
+          <p></p>
+        </section>
+        <section id="iletisim">
+          <h2>Iletisim</h2>
+          <p></p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2026 Gürkan Buğdaycıoğlu. Tüm hakları saklıdır.</p>
+    </footer>
+    </>
   )
 }
 
